94462c1d1b0b1947b4a663076333e20e86c7ec5f
masaruohashijr <masaru@vindixit.com>
2016-08-25 23:17:35 -0300

    first commit.

---
+++ src/main/java/com/vindixit/toggl/service/WorkShifts.java
@@ -0,0 +1,21 @@
+package com.vindixit.toggl.service;
+
+public class WorkShifts {
+
+	public static String getStartFirstShift(String[] periods){
+		return null;
+	}
+
+	public static String getEndFirstShift(String[] periods){
+		return null;
+	}
+	
+	public static String getStartSecondShift(String[] periods){
+		return null;
+	}
+	
+	public static String getEndSecondShift(String[] periods){
+		return null;
+	}
+	
+}
---
+++ src/test/java/com/vindixit/toggl/service/WorkShiftsTest.java
@@ -0,0 +1,14 @@
+package com.vindixit.toggl.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class WorkShiftsTest {
+
+	@Test
+	public void testGetStartFirstShift() {
+		fail("Not yet implemented");
+	}
+
+}
