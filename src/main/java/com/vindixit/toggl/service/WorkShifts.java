1687453cd154954264562f47b1554ed6a734c656
Masaru Ohashi Júnior <Masaru Ohashi Jnior@192.168.0.76>
2016-09-13 14:27:26 -0300

    verificacao1

--- src/main/java/com/vindixit/toggl/service/WorkShifts.java
+++ src/main/java/com/vindixit/toggl/service/WorkShifts.java
@@ -8,21 +8,13 @@ public class WorkShifts {
 		// Pretendemos um algoritmo de ordenação.
 		// Um valor máximo para hora para iniciarmos
 		// o algoritmo, pode ser o numero 25 (mesmo irreal).
-		
-		
-		// declarar uma variável auxiliar e inicializá-la com um valor imediatamente acima dos números possíveis para horário.
-		// coloquei 1000, mas está errado por não ser o valor imediatamente acima das opções de horas possíveis. CORRIGIR ISSO.
-		int least = 1000;
-		// Iterar o array de timeEntries e comparar a vairável auxiliar com a menor hora de um TimeEntry.
-		//for ...
-			//int start = timeEntries[i].getStart();
-			// comparar utilizando a estrutura IF
-			//if(...){
-				// substituir o valor da variável auxiliar pela menor hora do TimeEntry iterado.
-				//... 
-			//}
-		//}
-		// retornar a variável auxiliar.
+		int least = 25;
+		for (int i = 0; i < timeEntries.length; i++) {
+			int start = timeEntries[i].getStart();
+			if(start<least){
+				least = start; 
+			}
+		}
 		return least;
 	}
 
